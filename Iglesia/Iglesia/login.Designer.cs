b35a7ce2e4bb144d0fc8749b4b3a83371a8ba34f
julieta turco <julivickiturco23@gmail.com>
2023-11-07 00:51:36 -0300

    codigo de login insertado a medias

--- Iglesia/Iglesia/login.Designer.cs
+++ Iglesia/Iglesia/login.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button1 = new System.Windows.Forms.Button();
             this.pbOcultar = new System.Windows.Forms.PictureBox();
             this.pbMostrar = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pbOcultar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMostrar)).BeginInit();
@@ -46,20 +46,20 @@
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(311, 26);
-            this.textBox1.TabIndex = 1;
+            this.txtUsername.Location = new System.Drawing.Point(108, 77);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(311, 26);
+            this.txtUsername.TabIndex = 1;
             // 
-            // txtPass
+            // txtPassword
             // 
-            this.txtPass.Location = new System.Drawing.Point(108, 140);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(311, 26);
-            this.txtPass.TabIndex = 2;
-            this.txtPass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtPassword.Location = new System.Drawing.Point(108, 140);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(311, 26);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // linkLabel1
             // 
@@ -129,27 +129,27 @@
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
             // 
-            // btnIniciarSesion
+            // btnLogin
             // 
-            this.btnIniciarSesion.AutoEllipsis = true;
-            this.btnIniciarSesion.BackColor = System.Drawing.Color.SandyBrown;
-            this.btnIniciarSesion.BackgroundImage = global::Iglesia.Properties.Resources.logo;
-            this.btnIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnIniciarSesion.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnIniciarSesion.FlatAppearance.BorderSize = 5;
-            this.btnIniciarSesion.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.btnIniciarSesion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
-            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciarSesion.ForeColor = System.Drawing.Color.Black;
-            this.btnIniciarSesion.Location = new System.Drawing.Point(108, 213);
-            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(265, 65);
-            this.btnIniciarSesion.TabIndex = 3;
-            this.btnIniciarSesion.Text = "Iniciar Sesion";
-            this.btnIniciarSesion.UseVisualStyleBackColor = false;
-            this.btnIniciarSesion.Click += new System.EventHandler(this.button1_Click_1);
+            this.btnLogin.AutoEllipsis = true;
+            this.btnLogin.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnLogin.BackgroundImage = global::Iglesia.Properties.Resources.logo;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnLogin.FlatAppearance.BorderSize = 5;
+            this.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(108, 213);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(265, 65);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Iniciar Sesion";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // panel1
             // 
@@ -171,13 +171,13 @@
             this.CancelButton = this.button1;
             this.ClientSize = new System.Drawing.Size(490, 316);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.pbOcultar);
             this.Controls.Add(this.pbMostrar);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.btnIniciarSesion);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -186,7 +186,6 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Iglesia Adm.";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbOcultar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMostrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
@@ -198,13 +197,13 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pbMostrar;
         private System.Windows.Forms.PictureBox pbOcultar;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Panel panel1;
--- Iglesia/Iglesia/login.cs
+++ Iglesia/Iglesia/login.cs
@@ -24,225 +24,224 @@ namespace Iglesia
            // Form1.ShowDialog();
         }
 
-        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Acerca_de Form1 = new Acerca_de();
-            //Form1.ShowDialog();
-        }
+        //private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //Acerca_de Form1 = new Acerca_de();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void tesoreríaToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Tesorería Form1 = new Tesorería();
-            //Form1.ShowDialog();
-        }
+        //private void tesoreríaToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //Tesorería Form1 = new Tesorería();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void ensayosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Ensayos Form1 = new Ensayos();
-            //Form1.ShowDialog();
-        }
+        //private void ensayosToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //Ensayos Form1 = new Ensayos();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+        //private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    Close();
+        //}
 
-        private void asistenciaToolStripMenuItem3_Click(object sender, EventArgs e)
-        {
-           // AsistenciaEnsayos Form1 = new AsistenciaEnsayos();
-           // Form1.ShowDialog();
-        }
+        //private void asistenciaToolStripMenuItem3_Click(object sender, EventArgs e)
+        //{
+        //   // AsistenciaEnsayos Form1 = new AsistenciaEnsayos();
+        //   // Form1.ShowDialog();
+        //}
 
-        private void informesToolStripMenuItem4_Click(object sender, EventArgs e)
-        {
-            //CronogramaEnsayo Form1 = new CronogramaEnsayo();
-           // Form1.ShowDialog();
-        }
+        //private void informesToolStripMenuItem4_Click(object sender, EventArgs e)
+        //{
+        //    //CronogramaEnsayo Form1 = new CronogramaEnsayo();
+        //   // Form1.ShowDialog();
+        //}
 
-        private void limpiezaToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-           // Limpieza Form1 = new Limpieza();
-           // Form1.ShowDialog();
-        }
+        //private void limpiezaToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //   // Limpieza Form1 = new Limpieza();
+        //   // Form1.ShowDialog();
+        //}
 
-        private void cronogramaToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            //CronogramaLimpieza Form1 = new CronogramaLimpieza();
-            //Form1.ShowDialog();
-        }
+        //private void cronogramaToolStripMenuItem1_Click(object sender, EventArgs e)
+        //{
+        //    //CronogramaLimpieza Form1 = new CronogramaLimpieza();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void asistenciaToolStripMenuItem4_Click(object sender, EventArgs e)
-        {
-           // AsistenciaLimpieza Form1 = new AsistenciaLimpieza();
-           // Form1.ShowDialog();
-        }
+        //private void asistenciaToolStripMenuItem4_Click(object sender, EventArgs e)
+        //{
+        //   // AsistenciaLimpieza Form1 = new AsistenciaLimpieza();
+        //   // Form1.ShowDialog();
+        //}
 
-        private void informesToolStripMenuItem5_Click(object sender, EventArgs e)
-        {
-            //informeLimpieza Form1 = new informeLimpieza();
-            //Form1.ShowDialog();
-        }
+        //private void informesToolStripMenuItem5_Click(object sender, EventArgs e)
+        //{
+        //    //informeLimpieza Form1 = new informeLimpieza();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void asistenciasToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //AsistenciaCelulas Form1 = new AsistenciaCelulas();
-            //Form1.ShowDialog();
-        }
+        //private void asistenciasToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //AsistenciaCelulas Form1 = new AsistenciaCelulas();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void informeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //InformeCelulas Form1 = new InformeCelulas();
-            //Form1.ShowDialog();
-        }
+        //private void informeToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //InformeCelulas Form1 = new InformeCelulas();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void calendarioDeActividadesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //CalendarioActividades Form1 = new CalendarioActividades();
-            //Form1.ShowDialog();
-        }
+        //private void calendarioDeActividadesToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //CalendarioActividades Form1 = new CalendarioActividades();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void asistenciaToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //AsistenciaJovenes Form1 = new AsistenciaJovenes();
-           // Form1.ShowDialog();
-        }
+        //private void asistenciaToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //AsistenciaJovenes Form1 = new AsistenciaJovenes();
+        //   // Form1.ShowDialog();
+        //}
 
-        private void informesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //InformeJovenes Form1 = new InformeJovenes();
-            //Form1.ShowDialog();
-        }
+        //private void informesToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //InformeJovenes Form1 = new InformeJovenes();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void asistenciaToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            //AsistenciaNiños Form1 = new AsistenciaNiños();
-            //Form1.ShowDialog();
-        }
+        //private void asistenciaToolStripMenuItem1_Click(object sender, EventArgs e)
+        //{
+        //    //AsistenciaNiños Form1 = new AsistenciaNiños();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void informesToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            //InformeNiños Form1 = new InformeNiños();
-            //Form1.ShowDialog();
-        }
+        //private void informesToolStripMenuItem1_Click(object sender, EventArgs e)
+        //{
+        //    //InformeNiños Form1 = new InformeNiños();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void asistenciaToolStripMenuItem2_Click(object sender, EventArgs e)
-        {
-            //AsistenciaScautt Form1 = new AsistenciaScautt();
-            //Form1.ShowDialog();
-        }
+        //private void asistenciaToolStripMenuItem2_Click(object sender, EventArgs e)
+        //{
+        //    //AsistenciaScautt Form1 = new AsistenciaScautt();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void informesToolStripMenuItem2_Click(object sender, EventArgs e)
-        {
-            //InformeScautt Form1 = new InformeScautt();
-            //Form1.ShowDialog();
-        }
+        //private void informesToolStripMenuItem2_Click(object sender, EventArgs e)
+        //{
+        //    //InformeScautt Form1 = new InformeScautt();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void informesToolStripMenuItem3_Click(object sender, EventArgs e)
-        {
-           // AlabAdorInforme Form1 = new AlabAdorInforme();
-            //Form1.ShowDialog();
-        }
+        //private void informesToolStripMenuItem3_Click(object sender, EventArgs e)
+        //{
+        //   // AlabAdorInforme Form1 = new AlabAdorInforme();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void consultarPersonasToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //ConsultaPers Form1 = new ConsultaPers();
-            //Form1.ShowDialog();
-        }
+        //private void consultarPersonasToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    //ConsultaPers Form1 = new ConsultaPers();
+        //    //Form1.ShowDialog();
+        //}
 
-        private void cargarPersonasNuevasToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AgregarPersonas Form1 = new AgregarPersonas();
-            Form1.Show();
+        //private void cargarPersonasNuevasToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    AgregarPersonas Form1 = new AgregarPersonas();
+        //    Form1.Show();
 
-       
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-        }
+        //}
 
-        
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
 
-        private void dgvPersonas_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
 
-        }
+        //}
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            //GestionarPersonas Form1 = new GestionarPersonas();
-            //Form1.Show();
-        }
+        //private void button1_Click(object sender, EventArgs e)
+        //{
 
-        private void asignarMentorToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AsignarMentor Form1 = new AsignarMentor();
-            Form1.Show();
-        }
+        //}
 
-        private void modificarMentoresToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ModificarMentor Form1 = new ModificarMentor();
-            Form1.Show();
-        }
+        //private void dgvPersonas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        //{
 
-        private void modificarDatosPersonalesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Modificar_Personas Form1 = new Modificar_Personas();
-            Form1.Show();
-        }
+        //}
 
-        private void inhabilitarPersonasToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            InhabilitarPersonas Form1 = new InhabilitarPersonas();
-            Form1.Show();
-        }
+        //private void button1_Click_1(object sender, EventArgs e)
+        //{
+           
+        //}
 
-        private void altaDeMentoresToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AltaMentores Form1 =new AltaMentores();
-            Form1.Show();
-        }
+        //private void asignarMentorToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    AsignarMentor Form1 = new AsignarMentor();
+        //    Form1.Show();
+        //}
 
-        private void postularMiembrosADiscipuladoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            PostularADiscipulado Form1 = new PostularADiscipulado();
-            Form1.Show();
-        }
+        //private void modificarMentoresToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    ModificarMentor Form1 = new ModificarMentor();
+        //    Form1.Show();
+        //}
 
-        private void postulacionesPorRevisarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            PostulacionesRevisar Form1 = new PostulacionesRevisar();
-            Form1 .Show();
-        }
+        //private void modificarDatosPersonalesToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    Modificar_Personas Form1 = new Modificar_Personas();
+        //    Form1.Show();
+        //}
 
-        private void altaMinisteriosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AltaMinisterios Form1 = new AltaMinisterios();
-            Form1.Show();
-        }
+        //private void inhabilitarPersonasToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    InhabilitarPersonas Form1 = new InhabilitarPersonas();
+        //    Form1.Show();
+        //}
 
-        private void registroDeReunionesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            RegistroReunionesMentor Form1 = new RegistroReunionesMentor();
-            Form1.Show();
-        }
+        //private void altaDeMentoresToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    AltaMentores Form1 =new AltaMentores();
+        //    Form1.Show();
+        //}
 
-        private void altaUsuariosToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AltaUsuarios Form1 = new AltaUsuarios();
-            Form1.Show();
-        }
+        //private void postularMiembrosADiscipuladoToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    PostularADiscipulado Form1 = new PostularADiscipulado();
+        //    Form1.Show();
+        //}
+
+        //private void postulacionesPorRevisarToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    PostulacionesRevisar Form1 = new PostulacionesRevisar();
+        //    Form1 .Show();
+        //}
+
+        //private void altaMinisteriosToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    AltaMinisterios Form1 = new AltaMinisterios();
+        //    Form1.Show();
+        //}
+
+        //private void registroDeReunionesToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    RegistroReunionesMentor Form1 = new RegistroReunionesMentor();
+        //    Form1.Show();
+        //}
+
+        //private void altaUsuariosToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    AltaUsuarios Form1 = new AltaUsuarios();
+        //    Form1.Show();
+        //}
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
@@ -281,25 +280,57 @@ namespace Iglesia
 
         private void btnIniciarSesion_MouseEnter(object sender, EventArgs e)
         {
-            btnIniciarSesion.ForeColor = Color.White;
+            btnLogin.ForeColor = Color.White;
         }
 
         private void btnIniciarSesion_MouseLeave(object sender, EventArgs e)
         {
-            btnIniciarSesion.ForeColor = Color.Black;
+            btnLogin.ForeColor = Color.Black;
 
         }
 
         private void pbMostrar_Click(object sender, EventArgs e)
         {
             pbOcultar.BringToFront();
-            txtPass.PasswordChar = '\0';
+            txtPassword.PasswordChar = '\0';
         }
 
         private void pbOcultar_Click(object sender, EventArgs e)
         {
             pbMostrar.BringToFront();
-            txtPass.PasswordChar = '*';
+            txtPassword.PasswordChar = '*';
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Por favor, introduce tu nombre de usuario y contraseña.");
+                return;
+            }
+
+            bool areCredentialsValid = VerifyCredentials(username, password);
+            if (areCredentialsValid)
+            {
+                MessageBox.Show("¡Inicio de sesión exitoso!");
+                // Aquí puedes abrir la ventana principal de tu aplicación.
+            }
+            else
+            {
+                MessageBox.Show("Nombre de usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
+            }
+        }
+
+        private bool VerifyCredentials(string username, string password)
+        {
+            // Aquí debes implementar la lógica para verificar las credenciales del usuario.
+            // Esto podría implicar consultar una base de datos, un archivo, etc.
+            // Este método debe devolver 'true' si las credenciales son válidas y 'false' en caso contrario.
+            throw new NotImplementedException();
         }
     }
 }
+
